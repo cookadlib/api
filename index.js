a44ebe20023f7b38856c5cbbd38d7425a76969fc
Karl Podger <karl.podger@primeordinal.com>
2016-06-03 16:50:49 +0100

    Explore Koa 2

---
+++ index.js
@@ -0,0 +1,21 @@
+import Koa from 'koa';
+import {container} from 'needlepoint';
+
+const app = new Koa();
+
+// logger
+
+app.use(async (ctx, next) => {
+  const start = new Date;
+  await next();
+  const ms = new Date - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+
+app.use(ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
